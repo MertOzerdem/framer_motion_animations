4158dbd6f3554e06f6f9784d828a41c3daea742a
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-02-25 19:30:15 +0300

    CardList added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import WhellSelector from './components/WhellSelector/WhellSelector';
 import ItemShowcase from './views/ItemShowcase/ItemShowcase';
 import TabNavigation from './components/TabNavigation/TabNavigation';
+import CardList from './components/CardList/CardList';
+
+const animations = [0,1,2,3]
+
+const components = [
+  <WhellSelector />,
+  <ItemShowcase />,
+  <TabNavigation />,
+  <CardList />
+]
 
 function App() {
+  const [selected, setSelected] = useState(3);
+
+  const handleClick = (index: number) => {
+    setSelected(index);
+  }
+
+  console.log(components)
+
   return (
     <div className="App">
-      {false && <WhellSelector />}
+      <div>
+        {animations.map((_, index) => {
+          return (
+            <button key={index} onClick={() => handleClick(index)}>{index}</button>
+          )
+        })}
+      </div>
+      {components[selected]}
+      {/* {false && <WhellSelector />}
       {false && <ItemShowcase />}
-      {true && <TabNavigation />}
+      {false && <TabNavigation />}
+      {true && <CardList />} */}
     </div>
   );
 }
---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,89 @@
+import { AnimationProps, motion } from 'framer-motion'
+import { useState } from 'react'
+import styles from './CardList.module.scss'
+import Exclude from '../../images/Exclude.png'
+
+const bars = ['A', 'B', 'C']
+
+const variants = {
+    opened: {
+        opacity: 1,
+        scale: 1,
+        x: 10,
+        y: 10,
+        transition: {
+            duration: 0.3
+        }
+    },
+    closed: {
+        opacity: 0,
+        scale: 0,
+        x: 0,
+        y: 0,
+        transition: {
+            duration: 0.3
+        }
+    }
+}
+
+const CardList = () => {
+    const [selected, setSelected] = useState(-1)
+
+    const handleClick = (index: number) => {
+        if (index === selected) { 
+            setSelected(-1)
+        } else {
+            setSelected(index)
+        }
+    }
+
+    return (
+        <div className={`${styles.contentList}`}>
+            {bars.map((_, index) => {
+                return (
+                    <motion.div 
+                        className={`${styles.cardWrapper} ${selected === index ? styles.open : ''}`} 
+                        onClick={() => handleClick(index)}
+                        key={index}
+                        style={{ originX: 0, originY: 0 }} // not sure if this contributes to anyting
+                        transition={{ duration: 4 }}
+                        >
+                        <motion.div
+                            layout
+                            className={`${styles.card} ${styles['card--' + index]}`}
+                        >
+                            <TurnBack 
+                                className={`${styles.turnBackButton} ${styles['turnBackButton--' + index]}`} 
+                                variants={variants}
+                                isSelected={selected === index}/>
+                        </motion.div>
+                    </motion.div>
+                )
+            })}
+        </div>
+    )
+}
+
+interface SVGProps {
+    className: string,
+    variants: AnimationProps["variants"],
+    isSelected: boolean
+}
+
+const TurnBack = ({ className, variants, isSelected }: SVGProps) => {
+
+    return (
+        <motion.img
+            style={{ originX: 0, originY: 0 }}
+            src={Exclude} 
+            layout
+            className={className}
+            variants={variants}
+            initial={false}
+            animate={isSelected ? 'opened' : 'closed'}
+        />
+    )
+}
+
+export default CardList;
+
