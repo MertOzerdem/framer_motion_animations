07cce2572d242a19e1aa19e6c9168b83316acd7b
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-03-03 11:38:48 +0300

    Carousel seperated into modules

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,39 +1,32 @@
 import styles from './Carousel.module.scss'
-import { motion, MotionValue, ResolvedValues, useAnimation, useMotionValue, useTransform } from 'framer-motion'
-import { useEffect, useRef } from 'react';
-import { TransformProperties } from 'framer-motion/types/motion/types';
+import { motion, useAnimation, useMotionValue } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react';
+import Card from './Card/Card';
 
 const items = ['A', 'B', 'C']
 
-interface templateProps {
-    x: MotionValue<number>
-    rotateY: MotionValue<number>
+interface Shape {
+    marginRight: number,
+    width: number,
 }
 
 const Carousel = () => {
+    const [showCards, setShowCards] = useState(false)
+    const [shape, setShape] = useState<Shape>({marginRight: 0, width: 0})
     const wrapperRef = useRef<any>(null); // configure for TS later
     const controls = useAnimation();
     const x = useMotionValue(0);
-    const rotateYRange = [-45, 0, 45];
 
-    const xRange = [375/2, 0, -375/2];
-    const rotateY = useTransform(x, xRange, rotateYRange);
-    const scale = useTransform(x, xRange, [0.9, 1, 0.9]);
-    const transformOrigin = useTransform(x, latestX => {
-        latestX = -latestX;
-        console.log(latestX)
-        if(latestX >= 1) return '0% 50% 0px';
-        else return '100% 50% 0px';
-        // ['0% 50% 0px', '100% 50% 0px', '100% 50% 0px']
-    });
+    useEffect(() => {
+        setShape({
+            marginRight: Number(window.getComputedStyle(wrapperRef.current.childNodes[0]).getPropertyValue('margin-right').replace('px', '')),
+            width: wrapperRef.current.getBoundingClientRect().width
+        })
+    }, [])
 
-    // useEffect(() => {
-    //     xRange = [0, -wrapperRef.current.clientWidth];
-
-    //     rotateY = useTransform(x, xRange, rotateYRange);
-    //     scale = useTransform(x, xRange, [1, 0.95]);
-    //     transformOrigin = useTransform(x, xRange, ['0% 50% 0px', '100% 50% 0px'], { clamp: false });
-    // }, []);
+    useEffect(() => {
+        setShowCards(true)
+    }, [shape])
 
     const handleDragEnd = () => {
         if (wrapperRef.current) {
@@ -41,7 +34,7 @@ const Carousel = () => {
             let marginValue = Number(window.getComputedStyle(wrapperRef.current.childNodes[0]).getPropertyValue('margin-right').replace('px', ''));
             let targetItem = animationDirection * Number((x.get() / wrapperRef.current.clientWidth).toFixed());
             let maxNumberOfDragTarget = items.length - 1
-            
+
             if(maxNumberOfDragTarget < targetItem) {
                 targetItem = maxNumberOfDragTarget
             } else if (targetItem < 0) {
@@ -60,32 +53,28 @@ const Carousel = () => {
     }
 
     return (
-        <div className={styles.contentWrapper}>
-            <div className={styles.cardList} ref={wrapperRef}>
-                {items.map((_, index) => {
-                    return (
-                        <div 
-                            key={index}
-                            className={`${styles.cardWrapper}`}
-                        >
-                            <motion.div
-                                className={`${styles.card} ${styles['card--' + index]}`}
+        <>
+            <div className={styles.contentWrapper}>
+                <div className={styles.cardList} ref={wrapperRef}>
+                    {items.map((_, index) => {
+                        return (
+                            <motion.div 
+                                key={index}
+                                className={`${styles.cardWrapper}`}
                                 drag="x"
                                 dragMomentum={false}
                                 dragElastic={0.9}
-                                dragConstraints={wrapperRef}
                                 onDragEnd={handleDragEnd}
-                                transformTemplate={
-                                    // ({ x, rotateY }) => `perspective(200px) rotateY(${rotateY}) translateX(${x})`
-                                    ({ x, rotateY, scale }) => `perspective(400px) scale(${scale}) rotateY(${rotateY}) translateX(${x})`
-                                }
-                                style={{x, rotateY, scale, transformOrigin }}
                                 animate={controls}
-                            />
-                        </div>
-                    )})}
+                                style={{ x }}
+                            >
+                                {showCards && <Card x={x} index={index} marginRight={shape.marginRight} width={shape.width}/>}
+                            </motion.div>
+                        )})}
+                </div>
             </div>
-        </div>
+            <div className={styles.dummy}></div>
+        </>
     )
 }
 
