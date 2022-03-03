0fdcb2ed711e52f9d5353939f6cc7ba53f52b0ea
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-03-03 17:03:13 +0300

    Card last working version

--- src/components/Carousel/Card/Card.tsx
+++ src/components/Carousel/Card/Card.tsx
@@ -4,38 +4,26 @@ import { motion, MotionValue, useTransform } from 'framer-motion'
 interface CardProps {
     index: number,
     x: MotionValue<number>,
-    rotateYRange?: number[],
     marginRight: number,
     width: number
 }
 
-const Card = ({ index, x, rotateYRange, marginRight, width }:CardProps) => {
+const Card = ({ index, x, marginRight, width }:CardProps) => {
     const animationBias = -1;
     const length = marginRight + width
     const cardX = useTransform(x, latestX => {
         latestX = animationBias * latestX;
-        let leftSquareStart = latestX - (latestX/2)
-        let rightSquareEnd = latestX + (latestX/2)
-        let middlePoint = (index * length) + (width / 2)
+        const middlePoint = (index * length) + (width / 2)
 
-        index === 0 && console.log('latestX', latestX)
-        if (latestX < middlePoint - width) {
-            index === 0 && console.log('left')
-            return -width
-        } else if (latestX > middlePoint + width) {
-            index === 0 && console.log('right')
-            return width
-        }
-        else {
-            index === 0 && console.log('middle')
-            return (latestX) % length
-        }
+        if (latestX < middlePoint - width) return -width
+        else if (latestX > middlePoint + width) return width
+        else return latestX - (index * length)
 
         // let newX = (latestX - (index * marginRight)) % width
-        let newX = (latestX) % length
-        index === 1 && console.log('latestX', latestX)
-        index === 1 && console.log('newX', newX)
-        return newX
+        // let newX = (latestX) % length
+        // index === 1 && console.log('latestX', latestX)
+        // index === 1 && console.log('newX', newX)
+        // return newX
         
         // let leftSquareStart = latestX - (latestX/2)
         // let rightSquareEnd = latestX + (latestX/2)
@@ -49,26 +37,22 @@ const Card = ({ index, x, rotateYRange, marginRight, width }:CardProps) => {
         //     return animationBias * length
         // }
     });
-    const xRange = [-length/2, 0, length/2]; // doğru değiştirme bunu
-    // const xRange = [width, 0, -width];
-    rotateYRange = [-45, 0, 45]; // doğru değiştirme bunu
+    const xRange = [-length/2, 0, length/2];
+    const rotateYRange = [-45, 0, 45];
+    const scaleRange = [0.9, 1, 0.9]
     const rotateY = useTransform(cardX, xRange, rotateYRange);
-    const scale = useTransform(cardX, xRange, [0.9, 1, 0.9]);
+    const scale = useTransform(cardX, xRange, scaleRange);
     const transformOrigin = useTransform(x, latestX => {
         latestX = animationBias * latestX;
         let leftSide = (length * index);
-        // let rightSide = leftSide + width;
 
-        // index === 2 && console.log(leftSide)
-        // doğru değiştirme bunu
-        if (leftSide >= latestX) {
-            return '100% 50% 0px'
-        } else if (leftSide < latestX) {
-            return '0% 50% 0px'
-        }
-
-        // if(latestX >= 1) return '0% 50% 0px';
-        // else return '100% 50% 0px';
+        if (leftSide >= latestX) return '100% 50% 0px' 
+        else return '0% 50% 0px'
+        // if (leftSide >= latestX) {
+        //     return '100% 50% 0px'
+        // } else if (leftSide < latestX) {
+        //     return '0% 50% 0px'
+        // }
     });
 
     // useEffect(() => {
